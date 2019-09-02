fb15b94c94282f9161a92cfa2f89dc8f69a8ef23
Jesus Zuniga <jesus.zuniga@macys.com>
2019-09-02 11:13:22 -0400

    First commit

---
+++ src/main/java/com/nisum/stomas/demo/digitalproduct/Digitalproduct.java
@@ -0,0 +1,13 @@
+package com.nisum.stomas.demo.digitalproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Digitalproduct {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Digitalproduct.class, args);
+	}
+
+}
