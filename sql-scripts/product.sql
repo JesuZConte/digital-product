e58bfe685b64fd91b9a1c82d84c8e07549da37c5
Jesus Zuniga <jesus.zuniga@macys.com>
2019-09-03 16:54:35 -0400

    Fixing sql script

--- sql-scripts/product.sql
+++ sql-scripts/product.sql
@@ -4,15 +4,15 @@ USE `product_catalogue`;
 --
 -- Table structure for table `product`
 --
-
+DROP TABLE IF EXISTS `detail`;
 DROP TABLE IF EXISTS `product`;
 
 CREATE TABLE `product` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `product_url` varchar(45) DEFAULT NULL,
+  `product_url` varchar(100) DEFAULT NULL,
   `top_level_category_id` varchar(45) DEFAULT NULL,
   `top_level_category_name` varchar(45) DEFAULT NULL,
-  `price` decimal(11) DEFAULT NULL
+  `price` decimal(5,2) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
@@ -21,7 +21,7 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` VALUES 
-	(1310,'/shop/product/lenox-federal-platinum-5-piece-place-setting?ID=1310','53630','Fine China', 143.00);
+  (1310,'/shop/product/lenox-federal-platinum-5-piece-place-setting?ID=1310','53630','Fine China', 143.00);
 
 
 
@@ -29,19 +29,19 @@ DROP TABLE IF EXISTS `detail`;
 CREATE TABLE `detail` (
   `id` int(11) NOT NULL,
   `name` varchar(50) DEFAULT NULL,
-  `description` varchar(50) DEFAULT NULL,
+  `description` varchar(200) DEFAULT NULL,
   `has_video` tinyint(1) DEFAULT NULL,
-  `review_statistics` decimal(11) DEFAULT NULL,
+  `review_statistics` decimal(5,2) DEFAULT NULL,
   `brand` varchar(45) DEFAULT NULL,
   `upc_number` varchar(50) DEFAULT NULL,
   `available` tinyint(1) DEFAULT NULL,
-  `availability_message` varchar(50) DEFAULT NULL
+  `availability_message` varchar(50) DEFAULT NULL,
   UNIQUE KEY `detail_idx_1` (`id`),
-  CONSTRAINT `detail_ibfk_1` FOREIGN KEY (`id`) REFERENCES `users` (`id`)
+  CONSTRAINT `detail_ibfk_1` FOREIGN KEY (`id`) REFERENCES `product` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `authorities`
+-- Dumping data for table `detail`
 --
 
 INSERT INTO `detail` 
