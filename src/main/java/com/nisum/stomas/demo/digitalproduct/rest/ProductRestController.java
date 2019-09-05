dda0a7dba347b7beb2dd9b34471981ed12d54180
Jesus Zuniga <jesus.zuniga@macys.com>
2019-09-05 17:19:02 -0400

    Adding put mapping to rest controller

--- src/main/java/com/nisum/stomas/demo/digitalproduct/rest/ProductRestController.java
+++ src/main/java/com/nisum/stomas/demo/digitalproduct/rest/ProductRestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,6 +49,14 @@ public class ProductRestController {
     public Product addProduct(@RequestBody Product product) {
         product.setId(0);
 
+        productService.save(product);
+
+        return product;
+    }
+
+    // add mapping for PUT /products - update existing product
+    @PutMapping("/products")
+    public Product updateProduct(@RequestBody Product product) {
         productService.save(product);
 
         return product;
