a6cb12d97b0ebb1f6c2df3b7bcc815f27bd7c6e9
Jesus Zuniga <jesus.zuniga@macys.com>
2019-09-05 12:46:17 -0400

    Updating rest controller

--- src/main/java/com/nisum/stomas/demo/digitalproduct/rest/ProductRestController.java
+++ src/main/java/com/nisum/stomas/demo/digitalproduct/rest/ProductRestController.java
@@ -4,6 +4,7 @@ import com.nisum.stomas.demo.digitalproduct.entity.Product;
 import com.nisum.stomas.demo.digitalproduct.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,4 +29,15 @@ public class ProductRestController {
         return productService.findAll();
     }
 
+    // add mapping for GET /product/{productId}
+
+    @GetMapping("/products/{productId}")
+    public Product getProduct(@PathVariable int productId) {
+        Product product = productService.findById(productId);
+
+        if (product == null) {
+            throw new RuntimeException("Product id not found - " + productId);
+        }
+        return product;
+    }
 }
